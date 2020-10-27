46e28fe062e78b8b9f2245c70dafccce1672eaaa
heidy050505 <71679938+heidy050505@users.noreply.github.com>
2020-10-27 16:05:15 +0800

    新增起始頁面的程式檔

---
+++ 2D_game/Assets/Scrips/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    //按鈕透過方法(函式，功能)跟程式溝通
+    //語法:類型 方法名稱(){}
+    //void無類型
+    void GameStart()
+    {
+
+    }
+    void GameQuit()
+    {
+
+    }
+}
