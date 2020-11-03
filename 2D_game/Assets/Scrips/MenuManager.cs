0705018a1c599853f38cc3a47dd758b412bc5c0d
heidy050505 <71679938+heidy050505@users.noreply.github.com>
2020-11-03 16:03:06 +0800

    新增控制器程式

--- 2D_game/Assets/Scrips/MenuManager.cs
+++ 2D_game/Assets/Scrips/MenuManager.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
     //按鈕透過方法(函式，功能)跟程式溝通
     //語法:類型 方法名稱(){}
     //void無類型
-    void GameStart()
+    //修飾詞:public 公開    private 私人
+    public void GameStart()
     {
-
+        print("開始遊戲");
+        SceneManager.LoadScene("背景");    //字串
+        //SceneManager.LoadScene(1);    //int
     }
-    void GameQuit()
+    public void GameQuit()
     {
-
+        print("結束遊戲");
+        Application.Quit();
     }
 }
---
+++ 2D_game/Assets/Scrips/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //語法:
+    //修飾詞 類型 名稱 (指定 值);
+    public GameObject final;
+
+    //碰撞偵測條件
+    //兩個collider(碰撞)   一個rigidbody(鋼體)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("碰到傳送門");
+        final.SetActive(true);
+    }
+}
