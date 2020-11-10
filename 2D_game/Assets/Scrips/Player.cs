9a4df533de76d7ebb3799ed54c7a56a46e292255
heidy050505 <71679938+heidy050505@users.noreply.github.com>
2020-11-10 16:04:06 +0800

    角色控制及道具設定

--- 2D_game/Assets/Scrips/Player.cs
+++ 2D_game/Assets/Scrips/Player.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI; //引用 使用者介面
 
 public class Player : MonoBehaviour
 {
@@ -6,11 +7,34 @@ public class Player : MonoBehaviour
     //修飾詞 類型 名稱 (指定 值);
     public GameObject final;
 
+    //道具數量
+    public Text textCount;
+    //數量
+    public int count;
+
     //碰撞偵測條件
     //兩個collider(碰撞)   一個rigidbody(鋼體)
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)//collision儲存碰撞資訊
     {
-        print("碰到傳送門");
-        final.SetActive(true);
+
+        if (collision.name == "door")
+        {
+            final.SetActive(true);
+        }
+        //print("碰到傳送門");
+        //final.SetActive(true);
+
+
+        //如果碰到物件的標籤是櫻桃
+        if (collision.tag == "櫻桃")
+        {
+            //刪除(碰到的遊戲物件)
+            Destroy(collision.gameObject);
+
+            //遞增
+            count++;
+            //道具數量的文字 = "櫻桃數量:" + 數量
+            textCount.text = "櫻桃數量:" + count;
+        }
     }
 }
---
+++ 2D_game/Assets/Scrips/levelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelManager : MonoBehaviour
+{
+    //(參數1, 參數2, 參數3,...,參數N)
+    //語法
+    //類型 名稱
+    public void Nextlevel(string nameLV)
+    {
+        SceneManager.LoadScene(nameLV);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("選單");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+     }
+}
